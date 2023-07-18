0b718fd37cc15093e75dd8152975fd384a9372f6
Varad Joshi <varadvjoshi99@gmail.com>
2023-07-18 23:06:41 +0530

    changing to environment variables

--- index.js
+++ index.js
@@ -13,7 +13,12 @@ app.post("/", async (req, res) => {
   const { name, mobile } = req.body;
 
   const auth = new google.auth.GoogleAuth({
-    keyFile: "credentials.json",
+    credentials:{
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        client_email: process.env.GOOGLE_CLIENT_EMAIL,
+        project_id: process.env.GOOGLE_PROJECT_ID,
+        private_key: process.env.GOOGLE_PRIVATE_KEY      
+      },
     scopes: "https://www.googleapis.com/auth/spreadsheets",
   });
 
