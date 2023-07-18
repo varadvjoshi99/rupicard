e167ac5d4bf6a5cf624907a470332607d235717c
Varad Joshi <varadvjoshi99@gmail.com>
2023-07-18 23:41:22 +0530

    few path changes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const { google } = require("googleapis");
+const path = require("path");
 
 const app = express();
 app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
